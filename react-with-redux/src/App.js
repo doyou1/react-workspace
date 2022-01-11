9dd42838e4d67bd018ad0956c6a59d756b9a6db8
doyou1 <56281493+doyou1@users.noreply.github.com>
2022-01-11 21:06:20 +0900

    init redux-config

--- react-with-redux/src/App.js
+++ react-with-redux/src/App.js
@@ -4,15 +4,11 @@ import AddNumberRoot from './components/AddNumberRoot';
 import DisplayNumberRoot from './components/DisplayNumberRoot';
 
 const App = () => {
-  const [number, setNumber] = useState(5);
-
   return (
     <div className="App">
       <h1>Root</h1>
-      <AddNumberRoot onClick={function(size) {
-        setNumber((prev) => (prev + size));
-      }}/>
-      <DisplayNumberRoot number={number}/>
+      <AddNumberRoot />
+      <DisplayNumberRoot />
     </div>
   );
 }
