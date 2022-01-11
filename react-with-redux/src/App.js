fbe79c45296b32de47eef3109bb1d81b6d149542
doyou1 <56281493+doyou1@users.noreply.github.com>
2022-01-11 18:51:16 +0900

    init config for redux-basic

---
+++ react-with-redux/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import AddNumberRoot from './components/AddNumberRoot';
+import DisplayNumberRoot from './components/DisplayNumberRoot';
+
+const App = () => {
+  return (
+    <div className="App">
+      <h1>Root</h1>
+      <AddNumberRoot />
+      <DisplayNumberRoot />
+    </div>
+  );
+}
+
+export default App;
