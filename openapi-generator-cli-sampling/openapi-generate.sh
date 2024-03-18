9855efcd2cdbf2a5f018a572530892d983fdf805
doyou1 <tla149@daum.net>
2024-03-18 21:05:29 +0900

    generated

--- openapi-generator-cli-sampling/openapi-generate.sh
+++ openapi-generator-cli-sampling/openapi-generate.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-openapi-generator-cli generate -g typescript-angular -i ./references/api-with-examples.yaml -o ./generated
+openapi-generator-cli generate -g typescript-angular -i ./references/api-with-examples.yaml -o ./generated --global-property=models --additional-properties=modelPropertyNaming=camelCase --model-package=responses
+openapi-generator-cli generate -g typescript-angular -i ./references/api-with-examples.yaml -o ./generated --global-property=models --model-package=requests
